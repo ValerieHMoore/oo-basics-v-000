b5061cc5b65366596ecad18e4e5cb242528eb49e
Valerie Moore <periwinkle17@outlook.com>
2018-06-17 17:26:28 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,12 @@
 class Shoe
   attr_accessor:brand,:color,:size,:material,:condition
   
-  def initialize(brand)
+  def initialize(brand, color, size, material, condition)
     @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
   end
   
 end
