c03b1a8f6b95efc702a74580572153763ff34f43
Valerie Moore <periwinkle17@outlook.com>
2018-06-17 17:41:06 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,21 +5,5 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
-  
-  # def initialize(color)
-  #   @color = color
-  # end
-  
-  # def initialize(size)
-  #   @size = size
-  # end
-  
-  # def initialize(material)
-  #   @material = material
-  # end
-  
-  # def initialize(condition)
-  #   @condition = condition
-  # end
-  
+
 end
